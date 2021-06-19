5bc05d4cdebfe09a0de93052e4674f6bb70543d1
Denovan Niget <niget.denovan@gmail.com>
2021-06-19 16:43:24 +0200

    Tasks manager OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "tasks", to: "tasks#index", as: "tasks"  #For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Create
+  get "/tasks/new", to: "tasks#new", as: "new_task" #Afficher le form
+  post "/tasks", to: "tasks#create", as: "create" #Creer une nouvelle task
+  #Show
+  get "/tasks/:id", to: "tasks#show", as: "task" #Affiche les tasks
+  #Update
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit" #affiche le form de modif
+  patch "/tasks/:id", to: "tasks#update" #On patch les modif
+  #Delete
+  delete "tasks/:id", to: "tasks#destroy", as: "delete"
+  # resources :tasks
 end
